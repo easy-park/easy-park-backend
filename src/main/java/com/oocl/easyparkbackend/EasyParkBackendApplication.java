cfbdc3c2d8a5431bb51c5691c385c01066c32274
bbillymai <1363543881@qq.com>
2019-07-26 17:43:59 +0800

    first commit

---
+++ src/main/java/com/oocl/easyparkbackend/EasyParkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.oocl.easyparkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyParkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyParkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oocl/easyparkbackend/EasyParkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oocl.easyparkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyParkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
