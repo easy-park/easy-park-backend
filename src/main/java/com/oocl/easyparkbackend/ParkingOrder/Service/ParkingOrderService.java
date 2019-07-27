04fd8ceec3d5beb12fe2a761f2520e6a3e2bef96
LULI3 <luanyuanshizhu@126.com>
2019-07-27 21:42:40 +0800

    [Liam] init:update a method and add new method in ParkingOrderService the update test:findParkingOrderByStatus the add test:parkingLotListIsFull

--- src/main/java/com/oocl/easyparkbackend/ParkingOrder/Service/ParkingOrderService.java
+++ src/main/java/com/oocl/easyparkbackend/ParkingOrder/Service/ParkingOrderService.java
@@ -1,11 +1,15 @@
 package com.oocl.easyparkbackend.ParkingOrder.Service;
 
+import com.oocl.easyparkbackend.ParkingBoy.Entity.ParkingBoy;
+import com.oocl.easyparkbackend.ParkingBoy.Repository.ParkingBoyRepository;
+import com.oocl.easyparkbackend.ParkingLot.Entity.ParkingLot;
 import com.oocl.easyparkbackend.ParkingOrder.Entity.ParkingOrder;
 import com.oocl.easyparkbackend.ParkingOrder.Repository.ParkingOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ParkingOrderService {
@@ -13,7 +17,19 @@ public class ParkingOrderService {
     @Autowired
     private ParkingOrderRepository parkingOrderRepository;
 
-    public List<ParkingOrder> findParkingOrderByStatus(int status) {
-        return parkingOrderRepository.findAllByStatus(status);
+    @Autowired
+    private ParkingBoyRepository parkingBoyRepository;
+
+    public List<ParkingOrder> findParkingOrderByStatus(String id, int status) {
+        Optional<ParkingBoy> parkingBoy = parkingBoyRepository.findById(id);
+        List<ParkingLot> returnParkingLotList = parkingBoyRepository.findById("123").get().getParkingLotList();
+        if (status == 1 && parkingLotListIsFull(returnParkingLotList)) {
+            return null;
+        }
+        return parkingOrderRepository.findAllByParkingBoyAndStatus(parkingBoy.get(), status);
+    }
+
+    private boolean parkingLotListIsFull(List<ParkingLot> parkingLotList) {
+        return parkingLotList.stream().map(ParkingLot::getAvailable).reduce(0,(a,b) -> a + b)==0;
     }
 }
