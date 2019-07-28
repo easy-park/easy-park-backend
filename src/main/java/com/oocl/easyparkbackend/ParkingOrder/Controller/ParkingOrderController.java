3df6431e217d5ceed0fa7b171db03f2f52f6b940
bbillymai <1363543881@qq.com>
2019-07-28 11:23:01 +0800

    [Billy] init：ParkingBoyStatus and ParkingOrderStatus interface

--- src/main/java/com/oocl/easyparkbackend/ParkingOrder/Controller/ParkingOrderController.java
+++ src/main/java/com/oocl/easyparkbackend/ParkingOrder/Controller/ParkingOrderController.java
@@ -1,13 +1,9 @@
 package com.oocl.easyparkbackend.ParkingOrder.Controller;
 
-
 import com.oocl.easyparkbackend.ParkingOrder.Service.ParkingOrderService;
 import com.oocl.easyparkbackend.common.vo.ResponseVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
