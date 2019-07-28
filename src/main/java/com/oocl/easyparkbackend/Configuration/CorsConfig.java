991ec5662f5e9ba593d964ace7feda0fa7637c72
LIUST3 <stefan.liu@oocl.com>
2019-07-28 11:14:46 +0800

    [stefan] Integration: LightSecurity and init CorsConfig

---
+++ src/main/java/com/oocl/easyparkbackend/Configuration/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.oocl.easyparkbackend.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class CorsConfig extends WebMvcConfigurationSupport {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
