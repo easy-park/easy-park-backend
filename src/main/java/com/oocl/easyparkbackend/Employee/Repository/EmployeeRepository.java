71deb973ebe25ab893c30bfd4378fe465b3098c3
LULI3 <luanyuanshizhu@126.com>
2019-07-30 21:03:31 +0800

    [Liam] fix:fix some business bug

--- src/main/java/com/oocl/easyparkbackend/Employee/Repository/EmployeeRepository.java
+++ src/main/java/com/oocl/easyparkbackend/Employee/Repository/EmployeeRepository.java
@@ -4,7 +4,15 @@ import com.oocl.easyparkbackend.Employee.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    List<Employee> findByNameLike(String name);
+
+    List<Employee> findByEmailLike(String email);
+
+    List<Employee> findByPhoneNumberLike(String phone);
 
 }
