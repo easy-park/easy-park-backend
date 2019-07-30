5e113a9407cacddc0e3e16c9ef2d74356cac24dd
Amy oyzl <32362887+Amyoyzl@users.noreply.github.com>
2019-07-30 15:21:09 +0800

    [Amy] init: create method that find parkinglots by range

--- src/main/java/com/oocl/easyparkbackend/ParkingLot/Controller/ParkingLotController.java
+++ src/main/java/com/oocl/easyparkbackend/ParkingLot/Controller/ParkingLotController.java
@@ -3,6 +3,7 @@ package com.oocl.easyparkbackend.ParkingLot.Controller;
 import com.oocl.easyparkbackend.ParkingBoy.Exception.ParkingBoyIdErrorException;
 import com.oocl.easyparkbackend.ParkingLot.Entity.ParkingLot;
 import com.oocl.easyparkbackend.ParkingLot.Exception.ParkingLotNameAndCapacityNotNull;
+import com.oocl.easyparkbackend.ParkingLot.Exception.ParkingLotRangeErrorException;
 import com.oocl.easyparkbackend.ParkingLot.Service.ParkingLotService;
 import com.oocl.easyparkbackend.common.vo.ResponseVO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,12 @@ public class ParkingLotController {
         return ResponseVO.success(fetchedParkingLot);
     }
 
+    @GetMapping(path = "/parking_lots", params = {"start", "end"})
+    public ResponseVO getParkingLotsByRange(int start, int end) {
+        List<ParkingLot> parkingLots =  parkingLotService.getParkingLotsByRange(start, end);
+        return ResponseVO.success(parkingLots);
+    }
+
     @ExceptionHandler(ParkingBoyIdErrorException.class)
     public ResponseVO handleParkingBoyIdErrorException(ParkingBoyIdErrorException exception){
         return ResponseVO.serviceFail(exception.getMessage());
@@ -40,6 +47,11 @@ public class ParkingLotController {
 
     @ExceptionHandler(ParkingLotNameAndCapacityNotNull.class)
     public ResponseVO handleParkingLotNameAndCapacityNotNull(ParkingLotNameAndCapacityNotNull exception) {
+        return ResponseVO.serviceFail(exception.getMessage());
+    }
+
+    @ExceptionHandler(ParkingLotRangeErrorException.class)
+    public ResponseVO handleParkingLotRangeErrorException(ParkingLotRangeErrorException exception) {
         return ResponseVO.serviceFail(exception.getMessage());
     }
 
