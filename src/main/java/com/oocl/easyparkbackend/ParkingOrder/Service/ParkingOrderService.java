47a433084fc0270e6904b543a82456039f3ec01b
LULI3 <luanyuanshizhu@126.com>
2019-07-31 12:00:19 +0800

    [Liam]:fix: fix some business bug

--- src/main/java/com/oocl/easyparkbackend/ParkingOrder/Service/ParkingOrderService.java
+++ src/main/java/com/oocl/easyparkbackend/ParkingOrder/Service/ParkingOrderService.java
@@ -67,6 +67,7 @@ public class ParkingOrderService {
             return parkingOrderList;
         }
         parkingOrderList.addAll(parkingOrderRepository.findAllByStatus(status));
+        parkingOrderList.addAll(parkingOrderRepository.findAllByParkingBoyAndStatus(parkingBoy,2));
         return parkingOrderList;
     }
 
